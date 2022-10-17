83acd455d51416c4fdfb6eee6dc087adc1ce2ae9
储欣 <cccchu@163.com>
2022-10-17 13:27:00 +0800

    修改MySQL字符集

--- sql/order.sql
+++ sql/order.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `order` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT '订单ID，主键',
-  `order_no` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '订单号',
+  `order_no` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '订单号',
   `user_id` int NOT NULL COMMENT '用户ID',
   `product_id` int NOT NULL COMMENT '商品ID',
-  `product_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '商品名称',
+  `product_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '商品名称',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `nums` int NOT NULL,
